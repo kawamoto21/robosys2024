d1093415360bee85d8cf921d91d12f32cd8e19ff
Sou Kawamoto <s23c1040mc@s.chibakoudai.jp>
2025-01-11 03:49:24 +0900

    Add a command

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
-# robosys2024
-# plusコマンド
+# convert_and_reverseコマンド
 ![test](https://github.com/kawamoto/robosys2024/actions/workflows/test.yml/badge.svg)
 
-標準入力から読み込んだ数字を足す。
+標準入力されたものを変換する
 
-（ここに前ページの項目をいろいろ書く）
+1.大文字→小文字
+
+2.小文字→大文字
+
+3.文字列を逆転
+
+これら3つを同時に実行する。
+
+### 実行例
+$ echo "Hello World" | ./convert_and_reverse
+DLROw OLLEh
+
+
+
 
 ## 必要なソフトウェア
 - Python
   - テスト済みバージョン: 3.7〜3.10
 
 ## テスト環境
-- Ubuntu 24.04 LTS
+- Ubuntu 22.04 LTS
+
+- このソフトウェアパッケージは，3条項BSDライセンスの下，再頒布および使用が許可されます
 
 © 2024 Sou Kawamoto
--- test.bash
+++ test.bash
@@ -1,7 +1,40 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2024 Sou Kawamoto <s23c1040mc@s.chibakoudai.jp>
+# SPDX-License-Identifier: BSD-3-Clause
 
-a=山田
-[ "$a" = 上田 ]       
-echo $?               
-[ "$a" = 山田 ]
-echo $?
+# テストケースの定義
+test_cases=(
+    "Hello World"
+    "123 AbC"
+    "Test123"
+    "aBcDeFg"
+    " "
+)
+
+# 期待される出力の定義
+expected_outputs=(
+    "DLROw OLLEh"
+    "cBa 321"
+    "321tSET"
+    "GfEdCbA"
+    " "
+)
+
+# スクリプトのパス
+script_path="./convert_and_reverse.sh"
+
+# テストの実行
+for i in "${!test_cases[@]}"; do
+    input="${test_cases[$i]}"
+    expected="${expected_outputs[$i]}"
+    output=$(echo "$input" | $script_path)
+    
+    if [ "$output" == "$expected" ]; then
+        echo "Test $((i+1)): Passed"
+    else
+        echo "Test $((i+1)): Failed"
+        echo "  Input:    $input"
+        echo "  Expected: $expected"
+        echo "  Got:      $output"
+    fi
+done
