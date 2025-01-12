5b303f236ea8c81a8b7635ac7fc5ea1572a3b8ba
Sou Kawamoto <s23c1040mc@s.chibakoudai.jp>
2025-01-12 10:50:06 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -11,7 +11,7 @@ res=0
 
 # テストデータを用意
 input="Hello\nWorld\nPython\nTest"
-expected_output="OLLEh\nDLROw\nNOHTYp\nTSEt"
+expected_output=$(echo -e "OLLEh\nDLROw\nNOHTYp\nTSEt")
 
 # Pythonスクリプトの出力を取得
 output=$(echo -e "$input" | python3 convert_and_reverse.py)
